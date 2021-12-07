5227b6e9e72fe32ab237c7e387cdfc018e07e628
Leo Tran <leotran@c2r4p9.hive.fi>
2021-12-07 16:33:25 +0200

    moulichecks gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: leotran <leotran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 13:51:02 by leotran           #+#    #+#             */
-/*   Updated: 2021/12/06 17:13:42 by leotran          ###   ########.fr       */
+/*   Updated: 2021/12/07 15:38:44 by leotran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,13 @@ int	ft_getlinefromstatic(int fd, char **stathicc, char **line)
 	if (*(ft_strchr(stathicc[fd], NL) + 1) != '\0')
 	{
 		temp = ft_strdup(ft_strchr(stathicc[fd], NL) + 1);
-		free(stathicc[fd]);
+		ft_strdel(&stathicc[fd]);
 		stathicc[fd] = ft_strdup(temp);
-		free(temp);
-		temp = NULL;
+		ft_strdel(&temp);
 	}
 	else
 	{
-		free(stathicc[fd]);
-		stathicc[fd] = NULL;
+		ft_strdel(&stathicc[fd]);
 	}
 	return (1);
 }
@@ -42,11 +40,13 @@ void	ft_cpytostatic(int fd, char **stathicc, char *buffer)
 	if (stathicc[fd] != NULL)
 	{
 		temp = ft_strdup(stathicc[fd]);
-		free(stathicc[fd]);
+		ft_strdel(&stathicc[fd]);
 		stathicc[fd] = ft_strjoin(temp, buffer);
+		ft_strdel(&temp);
 	}
 	else
 		stathicc[fd] = ft_strdup(buffer);
+	ft_bzero(buffer, BUFF_SIZE + 1);
 }
 
 int	ft_cpyjoinfromstatic(int fd, char **stathicc, char *buffer, char **line)
@@ -54,16 +54,20 @@ int	ft_cpyjoinfromstatic(int fd, char **stathicc, char *buffer, char **line)
 	char	*temp;
 	char	*nextline;
 
-	if (stathicc[fd] != NULL)
+	if (stathicc[fd] != NULL && ft_strchr(stathicc[fd], NL) != NULL)
 	{
 		temp = ft_strccpy(buffer, NL);
 		nextline = ft_strjoin(stathicc[fd], temp);
-		free(stathicc[fd]);
+		ft_strdel(&stathicc[fd]);
+		ft_strdel(&temp);
 	}
-	else
+	else if (ft_strchr(buffer, NL) != NULL)
 		nextline = ft_strccpy(buffer, NL);
+	else
+		nextline = ft_strsub(stathicc[fd], 0, ft_strlen(stathicc[fd]));
 	stathicc[fd] = ft_strdup(ft_strchr(buffer, NL) + 1);
 	*line = nextline;
+	ft_bzero(buffer, BUFF_SIZE + 1);
 	return (1);
 }
 
@@ -72,32 +76,52 @@ int	ft_lastline(int fd, char **stathicc, char **line)
 	if (stathicc[fd] != NULL)
 	{
 		*line = ft_strsub(stathicc[fd], 0, ft_strlen(stathicc[fd]));
-		free(stathicc[fd]);
-		stathicc[fd] = NULL;
+		ft_strdel(&stathicc[fd]);
 		return (1);
 	}
-	return (0);
+	return (-1);
+}
+
+int	ft_readline(int fd, char **stathicc, char **line)
+{
+	int		i;
+	char	*buffer;
+
+	buffer = ft_strnew(BUFF_SIZE);
+	i = read(fd, buffer, BUFF_SIZE);
+	if (i < 0)
+		return (-1);
+		//printf("i = %d\n", i);
+	while (i > 0)
+	{
+		
+		if (ft_strchr(buffer, NL) == NULL && buffer[0] != '\0')
+		{
+			ft_cpytostatic(fd, stathicc, buffer);
+		//	printf("test\n");
+		}
+		if (buffer[0] == '\0')
+			i = 0;
+		else
+		{
+			return (ft_cpyjoinfromstatic(fd, stathicc, buffer, line));
+		//	printf("test1\n");
+		}
+	}
+	//printf("test\n");
+	return (ft_lastline(fd, stathicc, line));
 }
 
 int	get_next_line(const int fd, char **line)
 {
 	static char	*stathicc[4096];
-	char		*buffer;
 
-	buffer = ft_strnew(BUFF_SIZE);
+	if (fd < 0 || line == NULL)
+		return (-1);
 	if (stathicc[fd] != NULL && ft_strchr(stathicc[fd], NL) != NULL)
 		return (ft_getlinefromstatic(fd, stathicc, line));
 	else
 	{
-		if (fd < 0)
-			return (-1);
-		while (read(fd, buffer, BUFF_SIZE))
-		{
-			if (ft_strchr(buffer, NL) == NULL)
-				ft_cpytostatic(fd, stathicc, buffer);
-			else
-				return (ft_cpyjoinfromstatic(fd, stathicc, buffer, line));
-		}
+		return (ft_readline(fd, stathicc, line)); 
 	}
-	return (ft_lastline(fd, stathicc, line));
 }
--- main.c
+++ main.c
@@ -6,25 +6,36 @@
 /*   By: leotran <leotran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 20:02:42 by marvin            #+#    #+#             */
-/*   Updated: 2021/12/06 17:14:06 by leotran          ###   ########.fr       */
+/*   Updated: 2021/12/07 13:38:52 by leotran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-
 int	main(int argc, char **argv)
 {
 	int	fd = open("test.txt", O_RDONLY);
-	char	*line;
+	char	*line = NULL;
 	int fd2 = open(argv[1], O_RDONLY);
-	int fd3 = open("42", O_RDONLY);
+	int fd3 = open("42.txt", O_RDONLY);
 
+	//get_next_line(42, ((void *)0));
+	//get_next_line(42, &line);
+	
+	/*
+	get_next_line(fd, &line);
+	get_next_line(fd, &line);
+	get_next_line(fd, &line);
+	get_next_line(fd, &line); */
+
+	
 	while (get_next_line(fd, &line) == 1)
 	{
 		printf("%s\n", line);
 	}
+
+
 
 	return (0);
 }
