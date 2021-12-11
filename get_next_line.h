e15c62cd13f6d63391252c78239ccadb2e3d37e0
Leo Tran <leotran@c1r5p13.hive.fi>
2021-12-11 14:30:54 +0200

    removed unused header

--- get_next_line.h
+++ get_next_line.h
@@ -6,19 +6,18 @@
 /*   By: leotran <leotran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 02:43:39 by leo               #+#    #+#             */
-/*   Updated: 2021/12/11 14:03:30 by leotran          ###   ########.fr       */
+/*   Updated: 2021/12/11 14:30:07 by leotran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 100
+# define BUFF_SIZE 10000000
 # define FD_SIZE 4096
 # define NL '\n'
 
 # include <stdlib.h>
 # include <unistd.h>
-# include <fcntl.h>
 # include "libft/libft.h"
 
 int	get_next_line(const int fd, char **line);
