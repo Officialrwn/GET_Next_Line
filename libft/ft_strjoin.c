3d774f2b9ac6bcd5e22a9f4301ef7a4baf437e7f
Leo Tran <leotran@c2r4p9.hive.fi>
2021-12-20 15:18:19 +0200

    gnl update and tweaked libft

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: leotran <leotran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 13:26:36 by leotran           #+#    #+#             */
-/*   Updated: 2021/12/15 11:15:03 by leotran          ###   ########.fr       */
+/*   Updated: 2021/12/20 15:14:11 by leotran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,27 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	int		len;
 	char	*arr;
+	int		i;
 
+	i = 0;
 	if (s1 == NULL || s2 == NULL)
 		return (NULL);
 	len = ft_strlen(s1) + ft_strlen(s2);
-	arr = (char *)malloc(sizeof(char) * (len + 1));
+	arr = (char *)malloc(sizeof(char) * len + 1);
 	if (!arr)
 		return (NULL);
-	arr = ft_strcpy(arr, s1);
-	arr = ft_strcat(arr, s2);
+	len = 0;
+	while (s1[len])
+	{
+		arr[len] = s1[len];
+		len++;
+	}
+	while (s2[i])
+	{
+		arr[len] = s2[i];
+		i++;
+		len++;
+	}
+	arr[len] = '\0';
 	return (arr);
 }
